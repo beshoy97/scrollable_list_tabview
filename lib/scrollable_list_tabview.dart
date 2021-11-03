5d5f2df416a44a752dba0b3db28042c901d1660c
Beshoy Wagih <beshoy7991@yahoo.com>
2021-11-03 17:55:09 +0200

    add padding

--- lib/scrollable_list_tabview.dart
+++ lib/scrollable_list_tabview.dart
@@ -146,8 +146,7 @@ class _ScrollableListTabViewState extends State<ScrollableListTabView> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Padding(
-                  padding: _kTabMargin
-                      .add(const EdgeInsets.symmetric(vertical: 5.0)),
+                  padding: EdgeInsets.symmetric(vertical: 5.0),
                   child: _buildInnerTab(index),
                 ),
                 Flexible(
